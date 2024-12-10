225ac83874e0e06c1cfa4380d2b352e8c793cd2c
Othmaan Al-cheese <saraalenzii52@gmail.com>
2024-12-10 20:06:24 +0300

    recpes

--- apis/recipes/recipes.router.js
+++ apis/recipes/recipes.router.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const recipeController = require("../../apis/recipes/recipes.controller");
 
-router.get("/", recipeController.getAllRecipes);
+const {
+  getAllRecipes,
+  getRecipeById,
+  createRecipe,
+  recipeUpdate,
+  deleteRecipe,
+} = require("./recipes.controller");
 
-router.get("/:id", recipeController.getRecipeById);
+router.get("/", getAllRecipes);
 
-router.post("/", recipeController.createRecipe);
+router.get("/:id", getRecipeById);
+
+router.post("/", createRecipe);
+
+router.put("/:id", recipeUpdate);
+
+router.delete("/:id", deleteRecipe);
 
 module.exports = router;
