3c5cda458d33fac0a777038e5a638696c9adec45
mohamedshaban <sarahmama@moe.edu.kw>
2024-12-15 18:47:33 +0300

    updated

--- models/Ingredients.js
+++ models/Ingredients.js
@@ -1,10 +1,10 @@
 const { model, Schema } = require("mongoose");
 
 const IngredSchema = new Schema({
-    name: {
-        type: String,
-        default: null, 
-      }
+  name: {
+    type: String,
+    default: null,
+  },
 });
 
 module.exports = model("Ingred", IngredSchema);
